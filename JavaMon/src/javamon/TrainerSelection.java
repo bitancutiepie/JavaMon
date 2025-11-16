19b4037f697b2b606388c8ca1bef0011edbb7461
joshu <joshu@BITANCUTIE>
2025-11-16 20:20:06 +0800

    bitancor ready draft for TrainerSelection

--- JavaMon/src/javamon/TrainerSelection.java
+++ JavaMon/src/javamon/TrainerSelection.java
@@ -1,16 +1,24 @@
 package javamon;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TrainerSelection extends JFrame {
-	
 
-	public TrainerSelection() {
-        setTitle("Selection");
+    public TrainerSelection() {
+        setTitle("Trainer Selection");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+        setLayout(null); // manual placement
+
+        // HEADER IMAGE
+        ImageIcon titleIcon = new ImageIcon(getClass().getResource("/javamon/assets/title.png"));
+        JLabel titleLabel = new JLabel(titleIcon);
+        titleLabel.setBounds(0, 0, titleIcon.getIconWidth(), titleIcon.getIconHeight());
+        add(titleLabel);
+
         setVisible(true);
-	}
+    }
 }
